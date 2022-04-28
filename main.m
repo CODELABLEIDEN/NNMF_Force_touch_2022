595d516ba08fb3f6d96999f5e44ac5119122f2ee
Ruchella Kock <ruchella_kock@hotmail.com>
2022-04-28 17:34:33 +0200

    first commit

---
+++ main.m
@@ -0,0 +1,9 @@
+%% load the files
+% path to raw data
+processed_data_path = '/media/Storage/User_Specific_Data_Storage/ruchella/Feb_2022_BS_to_tap_classification_EEG';
+%%
+addpath(genpath('/home/ruchella/variable_activation'))
+addpath(genpath('/home/ruchella/imports'))
+addpath(genpath('/media/Storage/User_Specific_Data_Storage/ruchella/EEGsynclib_Mar_2022'))
+addpath(genpath(processed_data_path), '-end')
+%%
